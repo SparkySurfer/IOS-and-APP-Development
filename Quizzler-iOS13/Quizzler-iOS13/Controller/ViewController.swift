202478282b4e440410def36cbdf0852eb78d3670
SparkySurfer <90383068+SparkySurfer@users.noreply.github.com>
2023-01-22 16:15:40 +0000

    Updated controller

--- Quizzler-iOS13/Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Quizzler-iOS13/Controller/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var trueButton: UIButton!
@@ -30,27 +31,26 @@ override func viewDidLoad() {
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         let userAnswer = sender.currentTitle!
-        quizBrain.checkAnswer(userAnswer)
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
         
-        if userAnswer == actualAnswer {
+        if userGotItRight {
+            
             sender.backgroundColor = UIColor.green
             
         } else {
             sender.backgroundColor = UIColor.red
         }
         
-        if questionNumber == quiz.count-1{
-            questionNumber = 0
-        } else {
-            questionNumber += 1
-            progressBar.progress = (Float(questionNumber + 1)/Float(quiz.count))
-        }
+        quizBrain.nextQuestion()
+        
         Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
         
     }
     
     @objc func updateUI() {
-        questionLabel.text = quiz[questionNumber].text
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgess()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
         trueButton.backgroundColor = UIColor.clear
         falseButton.backgroundColor = UIColor.clear
 
