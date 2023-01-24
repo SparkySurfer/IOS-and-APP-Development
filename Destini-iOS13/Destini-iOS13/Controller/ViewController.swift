eb3373fba58b2eaa75d2abc442c2d15db7941c15
SparkySurfer <90383068+SparkySurfer@users.noreply.github.com>
2023-01-24 18:23:10 +0000

    Adding BMI calc and updated Destini app

--- Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
@@ -13,12 +13,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
+
+    
+  
+    
+    var storyBrain = StoryBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateUI()
     }
-
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        print(storyBrain.storyNumber)
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        print(storyBrain.storyNumber)
+        updateUI()
+        
+        
+      }
+    
+    func updateUI(){
+        storyLabel.text = storyBrain.stories[storyBrain.storyNumber].title
+        choice1Button.setTitle(storyBrain.stories[storyBrain.storyNumber].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.stories[storyBrain.storyNumber].choice2, for: .normal)
+    }
 
 }
 
--- Playgrounds/Functions.playground/Contents.swift
+++ Playgrounds/Functions.playground/Contents.swift
@@ -21,3 +21,29 @@ add(n1: 1, n2: 2)
 subtract(n1: 3, n2: 4)
 multiply(n1: 3, n2: 3)
 divide(n1: 3, n2: 3)
+
+
+func greetings(name: String) -> Bool {
+    if name == "Connor" || name == "Nicole"{
+        return true
+    } else {
+        return false
+    }
+}
+
+greetings(name: "Connor")
+
+var doorShouldOpen = greetings(name: "Oakley")
+
+print(doorShouldOpen)
+
+func isOdd(n:Int) -> Bool {
+    if n % 2 == 1 {
+        return true
+    } else {
+        return false
+    }
+    }
+
+
+
